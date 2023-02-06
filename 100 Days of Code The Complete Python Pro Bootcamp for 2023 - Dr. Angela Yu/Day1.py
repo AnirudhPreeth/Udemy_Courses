8fe39bc1e1c3a38f3fe57c80e145877487ded8c8
Anirudh P Preeth <anirudhpreeth@gmail.com>
2023-02-07 02:02:48 +0530

    Update.

--- 100 Days of Code The Complete Python Pro Bootcamp for 2023 - Dr. Angela Yu/Day1.py
+++ 100 Days of Code The Complete Python Pro Bootcamp for 2023 - Dr. Angela Yu/Day1.py
@@ -63,6 +63,7 @@ input("A prompt for the user")
 Thonny. 
 input() will get user input in console.
 Then print() will print the word "Hello" and the user input. 
+len function. 
 '''
 print("Hello " + input("What is your name?") + "!")
 
--- 100 Days of Code The Complete Python Pro Bootcamp for 2023 - Dr. Angela Yu/Day2.py
+++ 100 Days of Code The Complete Python Pro Bootcamp for 2023 - Dr. Angela Yu/Day2.py
@@ -1,3 +1,6 @@
 #Day 2.
 
+#Data Types, Numbers, Operations, Type Conversion, f-Strings.
+
+#Python Primitive Data Types
  
