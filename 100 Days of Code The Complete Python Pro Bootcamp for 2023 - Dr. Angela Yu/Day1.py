d3c0e456fc40d4e4a3c242673eda6360dd4ee0ed
Anirudh P Preeth <anirudhpreeth@gmail.com>
2023-02-06 20:59:14 +0530

    Updates.

--- 100 Days of Code The Complete Python Pro Bootcamp for 2023 - Dr. Angela Yu/Day1.py
+++ 100 Days of Code The Complete Python Pro Bootcamp for 2023 - Dr. Angela Yu/Day1.py
@@ -1,5 +1,40 @@
 #Day 1. 
 
-'''
+#Printing, Commenting, Debugging, String Manipulation and Variables. 
 
 '''
+print() function -> To print / output what you want to print. 
+Strings -> String of characters. Double quotes. "". 
+Syntax highlighting. 
+'''
+print("Hello World!")
+
+#[Interactive Coding Exercise] Printing. 
+#Exercise 1 - Printing. 
+print("Day 1 - Python Print Function")
+print("The function is declared like this:")
+print("print('what to print')")
+
+#The above is my solution to the problem. 
+#Answer:
+print("Day 1 - Python Print Function")
+print("The function is declared like this:")
+print("print('what to print')")
+
+'''
+String Manipulation and Code Intelligence -
+New Line -> \n.
+No gaps unless space is required for new line. 
+Concatenation -> Combine different strings so that they will be added to the end of another string. 
+Spaces are important in Python. 
+Indentation Error.  
+'''
+print("Hello World!\nHello World!")
+print("Hello" + "Angela") 
+print("Hello" + " Angela")
+print("Hello " + "Angela")
+print("Hello" +" "+ "Angela")
+
+
+
+
