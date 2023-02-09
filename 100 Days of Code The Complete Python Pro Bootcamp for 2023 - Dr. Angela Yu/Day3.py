cb302302a4c5bd34dba2d435f542b71848049b9d
Anirudh P Preeth <anirudhpreeth@gmail.com>
2023-02-09 11:54:04 +0530

    Update.

--- 100 Days of Code The Complete Python Pro Bootcamp for 2023 - Dr. Angela Yu/Day3.py
+++ 100 Days of Code The Complete Python Pro Bootcamp for 2023 - Dr. Angela Yu/Day3.py
@@ -137,4 +137,95 @@ else:
           
 #[Interactive Coding Exercise] Leap Year.             
 
+year = int(input("Which year do you want to check? "))
+if year %4 == 0 and year %400 == 0 and year %100 != 0:
+    print("Leap Year.")
+else: 
+    print("Not a Leap Year.")
+
+year = int(input("Which year do you want to check? "))
+if year %4 == 0 and year %400 == 0 or year %400 != 0:
+    print("Leap Year.")
+    if year %100 == 0:
+        print("Not Leap year.")
+else: 
+    print("Not Leap Year.")
+        
+        
+year = int(input("Which year do you want to check? "))
+if year %4 == 0 and year %400 == 0 or year %4 == 0 and year %400 !=0:
+    print("Leap Year.")
+    if year %100 == 0 and year%4 == 0 and year%400 != 0:
+        print("Not Leap year.")
+        if year %100 !=0 and year %4 == 0 and year %400 !=0:
+            print("Not Leap Year.")
+else: 
+    print("Not Leap Year.")
+            
+#The above is my solution to the problem. 
+#In the above, I documented my various test cases. 
+#In the last one, almost every year gave the right answer, except 2100. 
+#While the test case accepted the answer and it was a 100 per cent correct according to the test cases it used, it didn't use 2100. 
+#I'm not satisfied, and will check back on it later. 
+#Answer:
+year = int(input("Which year do you want to check? "))
+if year%4 == 0:
+    if year%100 == 0:
+        if year%400 == 0:
+            print("Leap Year.")
+        else:
+            print("Not leap year.")
+    else:
+        print("Leap Year.")
+else:
+    print("Not leap year.")
+    
+#Multiple If Statements in Succession.
+
+'''
+Multiple if.
+if condition1:
+  do A
+if condition2:
+  do B
+if condition3:
+  do C
+ 
+bill = bill + 3 is the same as bill +=3.  
+'''
+print("Welcome to the rollercoaster!")
+height = int(input("What is your height in cm? "))
+bill = 0
+if height >= 120:
+    print("You can ride the rollercoaster!")
+    age = int(input("What is your age?"))
+    if age < 12:
+        bill = 5
+        print("Child tickets are $5.")
+    elif age<=18:
+        bill = 7
+        print("Youth tickets are $7.")
+    else:
+        bill = 12
+        print("Adult tickets are $12.")    
+    
+    wants_photos = input("Do you want a photo taken? Y or N.")
+    if wants_photos == "Y":
+        bill += 3  
+    print(f"Your final bill will be: ${bill}.")                  
+else: 
+    print("Sorry, you have to grow taller before you can ride!")
+
+#[Interactive Coding Exercise] Pizza Order Practice.
+
+
+print("Welcome to Python Pizza Deliveries!")
+size = input("What size pizza do you want? S, M, or L ")
+add_pepperoni = input("Do you want pepperoni? Y or N ")
+extra_cheese = input("Do you want extra cheese? Y or N ")
+
+
+
+    
+
  
