f69c9a7d7f3dff03e080506a64767b756dafc28e
Anirudh P Preeth <anirudhpreeth@gmail.com>
2023-02-24 21:58:43 +0530

    Update.

---
+++ Artificial Intelligence A-Z™ Learn How To Build An AI/Section17.py
@@ -0,0 +1,21 @@
+# Section 17. 
+
+# Annex 1 : Artificial Neural Networks.
+
+'''
+Deep Learning ->
+Geoffrey Hinton - Godfather of Deep Learning. 
+Mimic how the human brain operates. 
+Artificial Neural Net. 
+Input Layer, Hidden Layer, Output Layer. All connected to each other. 
+Shallow Learning. 
+Lot and lots of hidden layers - Deep Learning. 
+
+Plan of Attack ->
+Neuron, Activation Function, How Neural networks work, How Neural Networks Learn, Gradient Descent, Stochastic Gradient Descent...
+...Back-propagation.
+
+Neuron ->
+Basic Building Block of Neural Networks. 
+
+'''
