4a052f230f760f094deb7d11d9e9033ec2a5666b
Anirudh P Preeth <anirudhpreeth@gmail.com>
2023-06-04 23:38:45 +0530

    Update.

---
+++ Beginning C++ Programming - From Beginner to Beyond - Tim Buchalka's Learn Programming Academy/1.cpp
@@ -0,0 +1,48 @@
+/*
+Extensions - .cpp and .h
+Object code. 
+Source Code -> C++ Compiler -> Object code -> Linker -> C++ Standard Library. 
+IDE. Cross Platform IDE. 
+*/
+
+#include<iostream>
+using namespace std;
+int main(){
+    cout<<"Hello Testing"<<endl;
+    return 0;
+}
+
+#include <iostream>
+int main() {
+    std::cout << "Hello world!";
+    return 0;
+}
+
+#include <iostream>
+using namespace std;
+int main() {
+    cout<<"Default Project"<<endl;
+    return 0;
+}
+
+#include <iostream>
+using namespace std;
+int main() {
+    cout<<"This is Project 2"<<endl;
+    return 0;
+}
+
+/*Command line interface on Windows.*/
+#include <iostream>
+int main() {
+    std::cout<<"Hello windows command line"<<std::endl;
+    return 0;
+}
+
+/*Command line interface on Ubuntu.*/
+#include <iostream>
+int main() {
+    std::cout<<"Hello Ubuntu command line"<<std::endl;
+    return 0;
+}
+
